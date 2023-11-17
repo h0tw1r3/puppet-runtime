642209734f893c75e1ddf0abf14402b1359352aa
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2023-11-17 11:43:03 -0600

    (maint) only force libxml2/libxslt internal mirror if necessary

--- configs/components/libxslt.rb
+++ configs/components/libxslt.rb
@@ -1,14 +1,7 @@
 component "libxslt" do |pkg, settings, platform|
   pkg.version '1.1.37'
-  pkg.sha256sum 'a4ecab265f44e888ed3b39e11c7e925103ef6e26e09d62e9381f26977df96343'
-  pkg.url "#{settings[:buildsources_url]}/libxslt-v#{pkg.get_version}.tar.gz"
-
-  # Newer versions of libxslt either ship as tar.xz or do not ship with a configure file
-  # and require a newer version of GNU Autotools to generate. This causes problems with
-  # the older and esoteric (AIX, Solaris) platforms that we support.
-  # So we generate a configure file manually, compress as tar.gz, and host internally.
-
-  pkg.build_requires "libxml2"
+  pkg.sha256sum 'b6f96869b8c42e8257b19d633d31e38cf12ff770829352a9dd109795ffc78bf2'
+  pkg.url "https://gitlab.gnome.org/GNOME/libxslt/-/archive/v#{pkg.get_version}/libxslt-v#{pkg.get_version}.tar.gz"
 
   if platform.is_aix?
     if platform.name == 'aix-7.1-ppc'
@@ -39,6 +32,33 @@ component "libxslt" do |pkg, settings, platform|
   else
     pkg.environment "LDFLAGS", settings[:ldflags]
     pkg.environment "CFLAGS", settings[:cflags]
+  end
+
+  build_deps = [ "libxml2" ]
+
+  if platform.is_sles?
+    build_deps << "autoconf"
+  elsif platform.is_deb? || platform.is_rpm?
+    build_deps << "dh-autoreconf"
+  end
+
+  if platform.name == 'el-8-x86_64' || platform.name == 'el-9-x86_64'
+    build_deps.reject! { |r| r == 'dh-autoreconf' }
+  end
+
+  build_deps.each do |dep|
+    pkg.build_requires dep
+  end
+
+  # Newer versions of libxslt either ship as tar.xz or do not ship with a configure file
+  # and require a newer version of GNU Autotools to generate. This causes problems with
+  # the older and esoteric (AIX, Solaris) platforms that we support.
+  # So we generate a configure file manually, compress as tar.gz, and host internally.
+  if (platform.is_aix? && platform.name == 'aix-7.1-ppc') || platform.is_solaris?
+    pkg.url "#{settings[:buildsources_url]}/libxslt-v#{pkg.get_version}.tar.gz"
+    pkg.sha256sum 'a4ecab265f44e888ed3b39e11c7e925103ef6e26e09d62e9381f26977df96343'
+  else
+    pkg.configure { ["autoreconf --force --install"] }
   end
 
   pkg.configure do
